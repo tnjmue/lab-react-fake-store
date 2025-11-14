788c123328fb41435f64cd99ac11255fc8044dd0
Tanja Mueller <tmueller19@gmx.de>
2025-11-14 20:27:16 +0100

    Solved Lab

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
@@ -9,15 +10,29 @@ function ProductDetailsPage() {
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
-
+  const { productId } = useParams()
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    console.log(productId)
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+    .then(response => response.json())
+    .then(prodData => setProduct(prodData))
+  }, [productId])
 
 
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      <article className="flex flex-col items-start g-5 p-5 card">
+            <img src={product.image} className="w-40 border-4 my-4" />
+            <span className="bg-blue-700 font-bold text-white px-2 my-2">{product.category}</span>
+            <h3 className="font-bold my-2" >{product.title}</h3>
+            <div className="flex gap-40">
+            <p className="text-left w-80">{product.description}</p>
+            <span className="text-blue-700 font-bold">${product.price}</span>
+            </div>
+      </article> 
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
@@ -7,11 +8,30 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+useEffect(() => {
+fetch("https://fakestoreapi.com/products")
+.then(response => response.json())
+.then(productsData => {
+  console.log(productsData)
+      setProducts(productsData)
+});
 
+}, []);
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {products.map((product) => (
+        <Link to={`/product/details/${product.id}`}>
+          <article key={product.id} className="product-row">
+            <img src={product.image} className="w-40 border-4" />
+            <div className="font-bold" >{product.title}</div>
+            <div>{product.category}</div>
+            <div>${product.price}</div>
+            <div className="w-80">{product.description}</div>
+          </article> 
+        </Link>
+      ))}
     </div>
   );
 }
